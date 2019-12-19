b187c4b9058a182b3eaea359e82d83dc26661d9a
Ram <ram_maddirala@gallup.com>
2019-12-19 16:03:41 -0600

    framework setup

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,5 +1,10 @@
-module.exports = function(app, db) {
-  app.post("/notes", (req, res) => {
-    res.send("Hello");
+module.exports = function (app, db) {
+  app.get('/notes/:id', (req, res) => { });
+  app.post('/notes', (req, res) => {
+    const note = { text: req.body.body, title: req.body.title };
+    db.collection('notes').insert(note, (err, result) => {
+      if (err) { res.send({ 'error': 'An error has occurred' }); }
+      else { res.send(result.ops[0]); }
+    });
   });
 };
