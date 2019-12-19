deef3d5c4873d1823aa15df6834da1077b262e85
Ram <ram_maddirala@gallup.com>
2019-12-19 15:54:24 -0600

    db connected

--- server.js
+++ server.js
@@ -6,9 +6,13 @@ const db = require('./config/db');
 const app = express();
 const port = 8000;
 
-MongoClient.connect(db.url, (err, database) => {  if (err) return console.log(err)})
+app.use(bodyParser.urlencoded({ extended: true }));
 
-require("./app/routes")(app, {});
+MongoClient.connect(db.url, (err, database) => { if (err) return console.log(err) })
+
+const database = database.db("note-api")
+
+require('./app/routes')(app, database);
 
 app.listen(port, () => {
   console.log("We are live on " + port);
