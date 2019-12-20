eb3a553cd97ccad78fed6a56a3fa5e8d6e67777b
Ram <ram_maddirala@gallup.com>
2019-12-20 13:48:10 -0600

    scaffolded

--- server.js
+++ server.js
@@ -12,12 +12,12 @@ MongoClient.connect(db.url, (err, database) => {
   if (err) {
     return console.log(err)
   }
+
+  const data = database.db("note-api")
+
+  require('./app/routes')(app, data);
+
+  app.listen(port, () => {
+    console.log("We are live on " + port);
+  });
 })
-
-const database = database.db("note-api")
-
-require('./app/routes')(app, database);
-
-app.listen(port, () => {
-  console.log("We are live on " + port);
-});
