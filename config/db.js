22594f5cdfb5127fd1fe0e9b4600a8326daf4668
ram <maddi011@umn.edu>
2019-12-21 16:46:04 -0600

    pruned

--- config/db.js
+++ config/db.js
@@ -1,1 +1,4 @@
-module.exports = { url: "mongodb+srv://ram_maddirala:yoyoyohinooo@clusteretf-eos5q.mongodb.net/test?retryWrites=true&w=majority" };
+module.exports = {
+  url:
+    "mongodb+srv://ram_maddirala:yoyoyohinooo@clusteretf-eos5q.mongodb.net/test?retryWrites=true&w=majority"
+};
